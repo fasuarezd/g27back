95e7f331f11c7fb74c7770b469ee8e7d4c0a7af7
unknown <fasuarezd@gmail.com>
2024-05-23 19:44:37 -0600

    CRUD de tareas OK

--- backend/controllers/tareasController.js
+++ backend/controllers/tareasController.js
@@ -1,7 +1,11 @@
 const asyncHandler = require('express-async-handler')
+const Tarea = require('../models/tareaModel')
 
 const getTareas = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: 'Get Tareas' })
+
+    const tareas = await Tarea.find()
+
+    res.status(200).json(tareas)
 })
 
 const crearTareas = asyncHandler(async (req, res) => {
@@ -11,15 +15,40 @@ const crearTareas = asyncHandler(async (req, res) => {
         throw new Error('No tecleaste el texto')
     }
 
-    res.status(201).json({ message: 'Crear Tareas' })
+    const tarea = await Tarea.create({
+        texto: req.body.texto
+    })
+
+    res.status(201).json(tarea)
 })
 
 const updateTareas = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Actualizar la tarea con id: ${req.params.id}` })
+
+    const tarea = await Tarea.findById(req.params.id)
+
+    if (!tarea) {
+        res.status(404)
+        throw new Error("La tarea especificada no existe")
+    }
+
+    const tareaUpdated = await Tarea.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json(tareaUpdated)
 })
 
 const deleteTareas = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Eliminar la tarea con id: ${req.params.id}` })
+
+    const tarea = await Tarea.findById(req.params.id)
+
+    if (!tarea) {
+        res.status(404)
+        throw new Error("La tarea especificada no existe")
+    }
+
+    //const tareaDeleted = await Tarea.findByIdAndDelete(req.params.id)
+    await tarea.deleteOne()
+
+    res.status(200).json({ id: req.params.id })
 })
 
 module.exports = {
