cb53cd04d3355c3436d4bdd136c4c5975eb47ba3
unknown <fasuarezd@gmail.com>
2024-05-30 22:11:12 -0600

    CORS

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,15 @@ const express = require('express')
 const dotenv = require('dotenv').config()
 const { errorHandler } = require('./middleware/errorMiddleware')
 const colors = require('colors')
+const cors = require('cors')
 const connectDB = require('./config/db')
 const port = process.env.PORT || 5000
 
 connectDB()
 
 const app = express()
+
+app.use(cors())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
